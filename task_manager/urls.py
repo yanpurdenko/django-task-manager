79f383fff95f2a118e796ce451b5f3e99f475721
Yan Purdenko <purdenko22@gmail.com>
2022-12-20 19:43:34 +0100

    create login.html and update main urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("app.urls", namespace="app")),
-]
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
