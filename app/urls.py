07e4a0790a316b6a7d765e9c9e0023af6a8e4097
Yan Purdenko <purdenko22@gmail.com>
2022-12-20 19:41:20 +0100

    create TaskListView, TaskCreateView and TaskUpdateView and create urls for them also add login_required feature

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
 from .views import index
+from .views import TaskCreateView, TaskListView, TaskUpdateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("tasks/", index, name="index")
-]
-
+    path("tasks/", index, name="index"),
+    path("tasks/critical/", TaskListView.as_view(), name="critical-task"),
+    path("tasks/create/", TaskCreateView.as_view(), name="create-task"),
+    path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="update-task"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = "app"
--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
 from django.shortcuts import render
 
-from app.models import Task
+from app.models import Task, Worker, TaskType
 
 
+@login_required
 def index(request):
     """View function for the home page of the site."""
 
@@ -16,3 +21,38 @@ def index(request):
     }
 
     return render(request, "app/index.html", context=context)
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    fields = ["name", "description", "deadline", "priority", "task_type", "assignees"]
+    template_name = "app/create_task_form.html"
+    success_url = reverse_lazy("app:index")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        priorities = Task.PRIORITY_CHOICES
+
+        context["priorities"] = [priority[1] for priority in priorities]
+        context["all_workers"] = Worker.objects.all()
+        context["all_task_types"] = TaskType.objects.all()
+        return context
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    template_name = "app/critical-tasks-list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # user = self.request.user
+
+        context["all_critical_tasks"] = Task.objects.filter(priority="Critical").prefetch_related()
+        return context
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("app:index")
