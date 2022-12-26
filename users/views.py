7cf7da927acabb9fa0b97a79d577896ca5294342
Yan Purdenko <purdenko22@gmail.com>
2022-12-26 02:23:14 +0100

    add profile update view

--- users/views.py
+++ users/views.py
@@ -1,32 +1,34 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from app.models import Worker
+from users.forms import UpdateUserForm, UpdateProfileForm
 
 
 @login_required()
 def profile_view(request):
     worker = Worker.objects.get(id=request.user.id)
+    profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
 
-    context = {
-        "worker": worker,
-    }
+    return render(request, "app/profile.html", {"worker": worker, "profile_form": profile_form})
 
-    return render(request, "app/profile.html", context=context)
 
-# @login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         user_form = UpdateUserForm(request.POST, instance=request.user)
-#         profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
-#
-#         if user_form.is_valid() and profile_form.is_valid():
-#             user_form.save()
-#             profile_form.save()
-#             messages.success(request, 'Your profile is updated successfully')
-#             return redirect(to='users-profile')
-#     else:
-#         user_form = UpdateUserForm(instance=request.user)
-#         profile_form = UpdateProfileForm(instance=request.user.profile)
-#
-#     return render(request, 'users/profile.html', {'user_form': user_form, 'profile_form': profile_form})
+@login_required
+def update_profile_view(request):
+    worker = Worker.objects.get(id=request.user.id)
+
+    if request.method == "POST":
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Your profile is updated successfully")
+            return redirect("users:worker-profile")
+
+        return render(request, "app/update_profile.html", {
+            "user_form": user_form, "profile_form": profile_form, "worker": worker
+        })
+    return render(request, "app/update_profile.html", {"worker": worker})
