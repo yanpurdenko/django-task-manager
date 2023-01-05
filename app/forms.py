34c96ac94128741d0f1a84b41e69e6f75a6d8dea
Yan Purdenko <purdenko22@gmail.com>
2023-01-05 22:24:29 +0100

    update task form to crispy forms

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
+from crispy_forms.helper import FormHelper
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-from app.models import Worker
+from app.models import Worker, Task
+from task_manager import settings
 
 
 class WorkerCreationForm(UserCreationForm):
@@ -11,5 +13,31 @@ class WorkerCreationForm(UserCreationForm):
         fields = UserCreationForm.Meta.fields + ("email", "first_name", "last_name", "position",)
 
 
-class TasksSearchForm(forms.Form):
-    name = forms.CharField(max_length=255, required=True)
+class CreateTaskForm(forms.ModelForm):
+
+    class Meta:
+        model = Task
+        fields = ("name", "description", "deadline", "priority", "task_type", "assignees",)
+        deadline = forms.DateField(widget=forms.DateInput(format="%d/%m/%Y"), input_formats=settings.DATE_INPUT_FORMATS)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "POST"
+        self.fields["description"].widget.attrs = {"rows": 3}
+        self.fields["deadline"].widget.attrs = {"type": "date"}
+
+
+class UpdateTaskForm(forms.ModelForm):
+
+    class Meta:
+        model = Task
+        fields = ("name", "description", "deadline", "priority", "task_type", "assignees",)
+        deadline = forms.DateField(widget=forms.DateInput(format="%d/%m/%Y"), input_formats=settings.DATE_INPUT_FORMATS)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "POST"
+        self.fields["description"].widget.attrs = {"rows": 3}
+        self.fields["deadline"].widget.attrs = {"type": "date"}
