c657d9a5896aba32730217a34921bf8c9bf1ca1e
Yan Purdenko <purdenko22@gmail.com>
2023-01-15 16:08:21 +0100

    add PhoneField in Profile model

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from app.models import Worker
 from .models import Profile
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class UpdateUserForm(forms.ModelForm):
@@ -19,12 +20,15 @@ class UpdateUserForm(forms.ModelForm):
 class UpdateProfileForm(forms.ModelForm):
     """UpdateForm class for update users profiles."""
 
-    avatar = forms.ImageField(widget=forms.FileInput(attrs={
-        "class": "btn btn-primary addButtonTask m-0", "placeholder": "", "value": ""
-    }))
-    phone = forms.CharField(max_length=255)
-    main_programming_language = forms.CharField(max_length=255)
+    avatar = forms.ImageField(required=False)
+    phone = PhoneNumberField()
+    main_programming_language = forms.CharField(max_length=255, required=False)
 
     class Meta:
         model = Profile
         fields = ["avatar", "phone", "main_programming_language"]
+        widgets = {
+            "avatar": forms.FileInput(
+                attrs={"class": "btn btn-primary addButtonTask m-0", "placeholder": "", "value": ""}
+            )
+        }
